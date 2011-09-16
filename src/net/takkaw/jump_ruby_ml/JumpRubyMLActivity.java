290f42303e4496a69c5a4b5199e0a6751a895719
takkaw <takkaw@gmail.com>
2011-09-16 21:05:02 +0900

    first commit

---
+++ src/net/takkaw/jump_ruby_ml/JumpRubyMLActivity.java
@@ -0,0 +1,13 @@
+package net.takkaw.jump_ruby_ml;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JumpRubyMLActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
