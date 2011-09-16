094f387104414df82eadbcf53833a4c15d7ed47b
takkaw <takkaw@gmail.com>
2011-09-16 21:47:24 +0900

    get Intent and display with Toast

--- src/net/takkaw/jump_ruby_ml/JumpRubyMLActivity.java
+++ src/net/takkaw/jump_ruby_ml/JumpRubyMLActivity.java
@@ -1,13 +1,27 @@
 package net.takkaw.jump_ruby_ml;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class JumpRubyMLActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	/** Called when the activity is first created. */
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//    	Log.v("test",getTweet());
+//    }
+	@Override
+	public void onStart(){
+		super.onStart();
+		Toast.makeText(this, getTweet(), Toast.LENGTH_SHORT).show();
+	}
+    
+	private String getTweet(){
+		Intent i = getIntent();
+		String tweet = i.getStringExtra(Intent.EXTRA_TEXT);
+		return tweet;
+	}
 }
