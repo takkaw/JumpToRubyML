03de2aba47e972fbe559e0b2cd53daf11239dae1
takkaw <takkaw@gmail.com>
2011-09-18 07:49:50 +0900

    did it

--- src/net/takkaw/jump_ruby_ml/JumpRubyMLActivity.java
+++ src/net/takkaw/jump_ruby_ml/JumpRubyMLActivity.java
@@ -1,27 +1,35 @@
 package net.takkaw.jump_ruby_ml;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class JumpRubyMLActivity extends Activity {
-
-	/** Called when the activity is first created. */
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-//    	Log.v("test",getTweet());
-//    }
+	final String  url_base = "http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-";
+	final String[] ml = {"list","dev","ext","math","talk","core"};
+		
 	@Override
-	public void onStart(){
+	protected void onStart(){
 		super.onStart();
-		Toast.makeText(this, getTweet(), Toast.LENGTH_SHORT).show();
+		String tweet = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+		Matcher m;
+		for(int i = 0; i < 6 ; i++ ){
+			m = Pattern.compile("ruby-" + ml[i] + ":(\\d+)").matcher(tweet);
+			if(m.find()){
+				startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(url_base + ml[i] + "/" + m.group(1))));
+				finish();
+				return;
+			}
+		}
+
+		Toast.makeText(this, "ML pattern not found.", Toast.LENGTH_SHORT).show();
+		finish();
+
+		return;
 	}
     
-	private String getTweet(){
-		Intent i = getIntent();
-		String tweet = i.getStringExtra(Intent.EXTRA_TEXT);
-		return tweet;
-	}
 }
